ee172ad1c82ca2907a388ea269c12ef9a05ecace
Kaua <kauaolima2007@gmail.com>
2026-03-26 20:02:56 -0300

    Branch certa, atividade

--- repositorioProfessor/frontendFaculdade2/praticas/pratica04/chronos-pomodoro/src/App.tsx
+++ repositorioProfessor/frontendFaculdade2/praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -1,10 +1,12 @@
+// App.jsx
+
+import './styles/theme.css'
+import './styles/global.css'
 export function App() {
   return (
-    <>
-      <>
-      <h1>Bem vindo a primeira aula de ReactJS</h1>
-      <h2>Nossa aula de introdução</h2>
-    </>
-    </>
-  )
+  <>
+    <h1>Olá, Mundo do React!</h1>
+    <p>Bem-vindo à nossa primeira aplicação React!</p>
+  </>
+  );
 }
