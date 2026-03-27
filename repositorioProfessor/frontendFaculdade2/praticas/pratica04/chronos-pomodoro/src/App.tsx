7688b15a0ad0300859f05d9b792dc6b15373983e
Kauã <kauaolima2007@gmail.com>
2026-03-26 23:37:10 -0300

    Finalizado Projeto Pomodoro

--- repositorioProfessor/frontendFaculdade2/praticas/pratica04/chronos-pomodoro/src/App.tsx
+++ repositorioProfessor/frontendFaculdade2/praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -1,6 +1,12 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer'; // <-- Importado!
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -11,9 +17,42 @@ export function App() {
       <Container>
         <Logo />
       </Container>
-
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            {/* Mantivemos apenas o botão principal de Play */}
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
+      </Container>
+
+      {/* Nosso novo rodapé entra aqui, no seu próprio Container! */}
+      <Container>
+        <Footer />
       </Container>
     </>
   );
